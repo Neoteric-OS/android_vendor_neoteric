98c4a070111b15568146a093dc64a945ece46ea2
Abhay Singh Gill <abhaygill017@gmail.com>
2025-09-20 12:59:49 +0530

    neoteric: Don't ship fs tools in ramdisk
    Breaks custom recoveries.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- target/product/neoteric-target.mk
+++ target/product/neoteric-target.mk
@@ -107,12 +107,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor
 
-# Include fs tools for dedicated recovery and ramdisk partitions.
-PRODUCT_PACKAGES += \
-    e2fsck_ramdisk \
-    resize2fs_ramdisk \
-    tune2fs_ramdisk
-
+# Include fs tools for dedicated recovery partition.
 PRODUCT_PACKAGES += \
     e2fsck.recovery \
     resize2fs.recovery \
