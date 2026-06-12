LineageOS/android_vendor_lineage

soong: Add mkdir module
Change-Id: I7b98cf5b120817a271036fca3aa0582d77861993

build/soong/mkdir/init.go
@@ -1,0 +1,29 @@
+// Copyright 2025 The LineageOS Project.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mkdir
+
+import (
+	"android/soong/android"
+)
+
+var pctx = android.NewPackageContext("lineage/soong/mkdir")
+
+func init() {
+	RegisterBuildComponents(android.InitRegistrationContext)
+}
+
+func RegisterBuildComponents(ctx android.RegistrationContext) {
+	ctx.RegisterModuleType("mkdir", MkdirFactory)
+}

Neoteric-OS/android_vendor_neoteric

Co-authored-by: LuK1337 <priv.luk@gmail.com>

soong/mkdir/init.go
@@ -1,0 +1,29 @@
+// Copyright 2025 The LineageOS Project.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mkdir
+
+import (
+	"android/soong/android"
+)
+
+var pctx = android.NewPackageContext("neoteric/soong/mkdir")
+
+func init() {
+	RegisterBuildComponents(android.InitRegistrationContext)
+}
+
+func RegisterBuildComponents(ctx android.RegistrationContext) {
+	ctx.RegisterModuleType("mkdir", MkdirFactory)
+}